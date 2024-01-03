aeaf341af7d4f6e04f9fb88912372b74905407fa
Mauro Gómez <m.gomeznitoni@gmail.com>
2024-01-03 20:42:06 +0000

    Final Commit (Path cost calculated)

--- run.py
+++ run.py
@@ -2,22 +2,31 @@
 
 import search
 
-ab = search.GPSProblem('O', 'E'
+ab = search.GPSProblem('A', 'B'
                        , search.romania)
 
 print("----------------------------------------------------------------------------------------------")
 print("Busqueda en amplitud:")
-print(search.breadth_first_graph_search(ab).path())
+result_path = search.breadth_first_graph_search(ab).path()
+print("Coste total:", str(result_path[0].path_cost))
+print(result_path)
 print("----------------------------------------------------------------------------------------------")
 print("Busqueda en profundidad:")
-print(search.depth_first_graph_search(ab).path())
+result_path = search.depth_first_graph_search(ab).path()
+print("Coste total:", str(result_path[0].path_cost))
+print(result_path)
 print("----------------------------------------------------------------------------------------------")
 print("Busqueda en ramificación y acotación:")
-print(search.branch_and_bound_graph_search(ab).path())
+result_path = search.branch_and_bound_graph_search(ab).path()
+print("Coste total:", str(result_path[0].path_cost))
+print(result_path)
 print("----------------------------------------------------------------------------------------------")
 print("Busqueda en ramificación y acotación subestimada:")
-print(search.branch_and_bound_subestimated_graph_search(ab).path())
+result_path = search.branch_and_bound_subestimated_graph_search(ab).path()
+print("Coste total:", str(result_path[0].path_cost))
+print(result_path)
 print("----------------------------------------------------------------------------------------------")
+
 
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
